8bcc10b6b3f813b7a6f502cb5c6c465bc7ab3011
Tom <tom.roger@epitech.eu>
2026-06-14 20:19:20 +0200

    feat: add process register access

--- include/corewar/process.h
+++ include/corewar/process.h
@@ -1,6 +1,7 @@
 #ifndef _PROCESS_H_
 #define _PROCESS_H_
 #include "corewar/arena.h"
+#include <stdbool.h>
 
 #define NUM_REGISTERS 16
 
@@ -12,5 +13,8 @@ typedef struct process_s {
 
 void process_init(process_t *process, int pc);
 void process_advance_pc(process_t *process, int offset);
+bool process_valid_register(int reg);
+unsigned int process_read_register(process_t *process, int reg);
+void process_write_register(process_t *process, int reg, unsigned int value);
 
 #endif // _PROCESS_H_
