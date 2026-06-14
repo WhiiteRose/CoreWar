cd96d74e92981d8fd715f0d804a91b35b88df362
Tom <tom.roger@epitech.eu>
2026-06-14 18:09:28 +0200

    feat: add arena multibyte read and write

--- src/vm/arena.c
+++ src/vm/arena.c
@@ -29,3 +29,37 @@ void arena_write_u8(arena_t *arena, int addr, unsigned char value)
     int wrapped_addr = arena_wrap_addr(addr);
     arena->memory[wrapped_addr] = value;
 }
+
+unsigned short arena_read_u16(arena_t *arena, int addr)
+{
+    int wrapped_addr = arena_wrap_addr(addr);
+    unsigned char high_byte = arena->memory[wrapped_addr];
+    unsigned char low_byte = arena->memory[arena_wrap_addr(wrapped_addr + 1)];
+    return (high_byte << 8) | low_byte;
+}
+
+void arena_write_u16(arena_t *arena, int addr, unsigned short value)
+{
+    int wrapped_addr = arena_wrap_addr(addr);
+    arena->memory[wrapped_addr] = (value >> 8) & 0xFF;
+    arena->memory[arena_wrap_addr(wrapped_addr + 1)] = value & 0xFF;
+}
+
+unsigned int arena_read_u32(arena_t *arena, int addr)
+{
+    int wrapped_addr = arena_wrap_addr(addr);
+    unsigned char b0 = arena->memory[wrapped_addr];
+    unsigned char b1 = arena->memory[arena_wrap_addr(wrapped_addr + 1)];
+    unsigned char b2 = arena->memory[arena_wrap_addr(wrapped_addr + 2)];
+    unsigned char b3 = arena->memory[arena_wrap_addr(wrapped_addr + 3)];
+    return (b0 << 24) | (b1 << 16) | (b2 << 8) | b3;
+}
+
+void arena_write_u32(arena_t *arena, int addr, unsigned int value)
+{
+    int wrapped_addr = arena_wrap_addr(addr);
+    arena->memory[wrapped_addr] = (value >> 24) & 0xFF;
+    arena->memory[arena_wrap_addr(wrapped_addr + 1)] = (value >> 16) & 0xFF;
+    arena->memory[arena_wrap_addr(wrapped_addr + 2)] = (value >> 8) & 0xFF;
+    arena->memory[arena_wrap_addr(wrapped_addr + 3)] = value & 0xFF;
+}
--- tests/unit/test_arena.c
+++ tests/unit/test_arena.c
@@ -1,19 +1,19 @@
 #include <criterion/criterion.h>
 #include "corewar/arena.h"
 
-Test(arena_wrap_addr, keeps_valid_adress) {
+Test(arena_wrap_addr, keeps_valid_address) {
     cr_assert_eq(arena_wrap_addr(0), 0);
     cr_assert_eq(arena_wrap_addr(1), 1);
     cr_assert_eq(arena_wrap_addr(4095), 4095);
 }
 
-Test(arena_wrap_addr, wraps_adresses_after_memory_end) {
+Test(arena_wrap_addr, wraps_addresses_after_memory_end) {
     cr_assert_eq(arena_wrap_addr(4097), 1);
     cr_assert_eq(arena_wrap_addr(4098), 2);
     cr_assert_eq(arena_wrap_addr(8192), 0);
 }
 
-Test(arena_wrap_addr, wraps_negative_adresses) {
+Test(arena_wrap_addr, wraps_negative_addresses) {
     cr_assert_eq(arena_wrap_addr(-1), 4095);
     cr_assert_eq(arena_wrap_addr(-2), 4094);
     cr_assert_eq(arena_wrap_addr(-4096), 0);
@@ -44,6 +44,14 @@ Test (arena_read_u8, reads_value_from_wrapped_address) {
     cr_assert_eq(arena_read_u8(&arena, 4096), 42);
 }
 
+Test(arena_read_u8, reads_value_from_negative_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena.memory[4095] = 42;
+
+    cr_assert_eq(arena_read_u8(&arena, -1), 42);
+}
+
 Test(arena_write_u8, writes_value_to_arena) {
     arena_t arena;
     arena_init(&arena);
@@ -58,4 +66,132 @@ Test(arena_write_u8, writes_value_to_wrapped_address) {
     arena_write_u8(&arena, 4096, 42);
 
     cr_assert_eq(arena.memory[0], 42);
+}
+
+Test(arena_write_u8, writes_value_to_negative_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena_write_u8(&arena, -1, 42);
+
+    cr_assert_eq(arena.memory[4095], 42);
+}
+
+Test(arena_write_u16, writes_value_to_arena) {
+    arena_t arena;
+    arena_init(&arena);
+    arena_write_u16(&arena, 0, 0x1234);
+
+    cr_assert_eq(arena.memory[0], 0x12);
+    cr_assert_eq(arena.memory[1], 0x34);
+}
+
+Test(arena_write_u16, writes_value_to_wrapped_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena_write_u16(&arena, 4095, 0x1234);
+
+    cr_assert_eq(arena.memory[4095], 0x12);
+    cr_assert_eq(arena.memory[0], 0x34);
+}
+
+Test(arena_write_u16, writes_value_to_negative_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena_write_u16(&arena, -1, 0x1234);
+
+    cr_assert_eq(arena.memory[4095], 0x12);
+    cr_assert_eq(arena.memory[0], 0x34);
+}
+
+Test(arena_read_u16, reads_value_from_arena) {
+    arena_t arena;
+    arena_init(&arena);
+    arena.memory[0] = 0x12;
+    arena.memory[1] = 0x34;
+
+    cr_assert_eq(arena_read_u16(&arena, 0), 0x1234);
+}
+
+Test(arena_read_u16, reads_value_from_wrapped_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena.memory[4095] = 0x12;
+    arena.memory[0] = 0x34;
+
+    cr_assert_eq(arena_read_u16(&arena, 4095), 0x1234);
+}
+
+Test(arena_read_u16, reads_value_from_negative_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena.memory[4095] = 0x12;
+    arena.memory[0] = 0x34;
+
+    cr_assert_eq(arena_read_u16(&arena, -1), 0x1234);
+}
+
+Test(arena_read_u32, reads_value_from_arena) {
+    arena_t arena;
+    arena_init(&arena);
+    arena.memory[0] = 0x12;
+    arena.memory[1] = 0x34;
+    arena.memory[2] = 0x56;
+    arena.memory[3] = 0x78;
+
+    cr_assert_eq(arena_read_u32(&arena, 0), 0x12345678);
+}
+
+Test(arena_read_u32, reads_value_from_wrapped_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena.memory[4095] = 0x12;
+    arena.memory[0] = 0x34;
+    arena.memory[1] = 0x56;
+    arena.memory[2] = 0x78;
+
+    cr_assert_eq(arena_read_u32(&arena, 4095), 0x12345678);
+}
+
+Test(arena_read_u32, reads_value_from_negative_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena.memory[4095] = 0x12;
+    arena.memory[0] = 0x34;
+    arena.memory[1] = 0x56;
+    arena.memory[2] = 0x78;
+
+    cr_assert_eq(arena_read_u32(&arena, -1), 0x12345678);
+}
+
+Test(arena_write_u32, writes_value_to_arena) {
+    arena_t arena;
+    arena_init(&arena);
+    arena_write_u32(&arena, 0, 0x12345678);
+
+    cr_assert_eq(arena.memory[0], 0x12);
+    cr_assert_eq(arena.memory[1], 0x34);
+    cr_assert_eq(arena.memory[2], 0x56);
+    cr_assert_eq(arena.memory[3], 0x78);
+}
+
+Test(arena_write_u32, writes_value_to_wrapped_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena_write_u32(&arena, 4095, 0x12345678);
+
+    cr_assert_eq(arena.memory[4095], 0x12);
+    cr_assert_eq(arena.memory[0], 0x34);
+    cr_assert_eq(arena.memory[1], 0x56);
+    cr_assert_eq(arena.memory[2], 0x78);
+}
+
+Test(arena_write_u32, writes_value_to_negative_address) {
+    arena_t arena;
+    arena_init(&arena);
+    arena_write_u32(&arena, -1, 0x12345678);
+
+    cr_assert_eq(arena.memory[4095], 0x12);
+    cr_assert_eq(arena.memory[0], 0x34);
+    cr_assert_eq(arena.memory[1], 0x56);
+    cr_assert_eq(arena.memory[2], 0x78);
 }
