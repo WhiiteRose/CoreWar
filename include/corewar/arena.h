164019b0f92900b83c615596748098b7deb4a589
Tom <tom.roger@epitech.eu>
2026-06-15 19:19:31 +0200

    refactor: use fixed-width integer types

--- include/corewar/arena.h
+++ include/corewar/arena.h
@@ -1,18 +1,20 @@
 #ifndef _ARENA_H_
 #define _ARENA_H_
 
+#include <stdint.h>
+
 #define MEM_SIZE 4096
 
 typedef struct arena_s {
-    unsigned char memory[MEM_SIZE];
+    uint8_t memory[MEM_SIZE];
 } arena_t;
 
 int arena_wrap_addr(int addr);
 void arena_init(arena_t *arena);
-unsigned char arena_read_u8(arena_t *arena, int addr);
-void arena_write_u8(arena_t *arena, int addr, unsigned char value);
-unsigned short arena_read_u16(arena_t *arena, int addr);
-void arena_write_u16(arena_t *arena, int addr, unsigned short value);
-unsigned int arena_read_u32(arena_t *arena, int addr);
-void arena_write_u32(arena_t *arena, int addr, unsigned int value);
+uint8_t arena_read_u8(arena_t *arena, int addr);
+void arena_write_u8(arena_t *arena, int addr, uint8_t value);
+uint16_t arena_read_u16(arena_t *arena, int addr);
+void arena_write_u16(arena_t *arena, int addr, uint16_t value);
+uint32_t arena_read_u32(arena_t *arena, int addr);
+void arena_write_u32(arena_t *arena, int addr, uint32_t value);
 #endif // _ARENA_H_
--- include/corewar/process.h
+++ include/corewar/process.h
@@ -2,19 +2,20 @@
 #define _PROCESS_H_
 #include "corewar/arena.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 #define NUM_REGISTERS 16
 
 typedef struct process_s {
     int pc;
     int carry;
-    unsigned int registers[NUM_REGISTERS];
+    uint32_t registers[NUM_REGISTERS];
 } process_t;
 
 void process_init(process_t *process, int pc);
 void process_advance_pc(process_t *process, int offset);
 bool process_valid_register(int reg);
-unsigned int process_read_register(process_t *process, int reg);
-void process_write_register(process_t *process, int reg, unsigned int value);
+uint32_t process_read_register(process_t *process, int reg);
+void process_write_register(process_t *process, int reg, uint32_t value);
 
 #endif // _PROCESS_H_
