8e7d6da31c5b44284bff0c2910a9872cc24f3b28
Tom <tom.roger@epitech.eu>
2026-07-19 13:15:20 +0200

    feat: add argument type size helper

--- tests/unit/test_coding_byte.c
+++ tests/unit/test_coding_byte.c
@@ -22,3 +22,19 @@ Test(coding_byte_get_arg_type, returns_ind_for_arg_index_3_with_coding_byte_0x90
     cr_assert_eq(coding_byte_get_arg_type(0x90, 2), ARG_REG);
     cr_assert_eq(coding_byte_get_arg_type(0x90, 3), ARG_NONE);
 }
+
+Test(arg_type_size, returns_1_for_arg_reg) {
+    cr_assert_eq(arg_type_size(ARG_REG), 1);
+}
+
+Test(arg_type_size, returns_4_for_arg_dir) {
+    cr_assert_eq(arg_type_size(ARG_DIR), 4);
+}
+
+Test(arg_type_size, returns_2_for_arg_ind) {
+    cr_assert_eq(arg_type_size(ARG_IND), 2);
+}
+
+Test(arg_type_size, returns_0_for_arg_none) {
+    cr_assert_eq(arg_type_size(ARG_NONE), 0);
+}
